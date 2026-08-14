313524649b2ff0aab5db9cd2fdc3729d0c59379e
alexsuitman <alecub96@hotmail.com>
2026-08-14 02:50:01 +0100

    Corrige contraseña de MariaDB eliminando la barra baja final

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -19,7 +19,7 @@ function getMariaDbCredentials() {
     host: '127.0.0.1',
     port: 3306,
     user: 'u896809627_vaneando',
-    password: 'Sillaman1537912537912_',
+    password: 'Sillaman1537912537912',
     database: 'u896809627_vaneando',
   };
 }
